aeffb6a0f3d754409128975cf1ba88154fc685cb
unknown <designanimation.inc@gmail.com>
2024-01-11 11:49:54 -0300

    feat: configuration ms-users

--- src/main/java/com/bench/apigateway/filters/GlobalFilterGateway.java
+++ src/main/java/com/bench/apigateway/filters/GlobalFilterGateway.java
@@ -2,11 +2,11 @@ package com.bench.apigateway.filters;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
-import org.springframework.cloud.gateway.filter.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -19,17 +19,23 @@ public class GlobalFilterGateway implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Pre Filter: Request");
-        //exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
+        logger.info("PRE Filter: Request");
+        exchange.getRequest().mutate().headers(header ->
+                header.add("token", "123456"));
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info("Post Filter: Response");
+            logger.info("POST Filter: Response");
 
-//            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
-//                exchange.getResponse().getHeaders().add("token", valor);
-//            });
+            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token"))
+                    .ifPresent(token -> {
+                        exchange.getResponse().getHeaders()
+                                .add("token", token);
+                    });
 
-            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+            exchange.getResponse().getCookies()
+                    .add("color", ResponseCookie.from("color", "red").build());
+
+            //exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
     }
 
